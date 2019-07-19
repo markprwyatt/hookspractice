d471b8bb6b3b58c766ff67cca90f09e40b73bba9
Mark Wyatt <markprwyatt@gmail.com>
2019-07-19 13:52:56 +0100

    basic react hooks practice

---
+++ src/components/UserList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import useResources from "./useResources";
+
+const UserList = () => {
+  const resources = useResources("users");
+
+  return (
+    <ul>
+      {resources.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
